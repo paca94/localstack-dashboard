368790d05f1003a76e2e76d0c302468f9b2f493e
JihwanKim <jihwan.dev@gmail.com>
2022-09-02 00:47:41 +0900

    sqs service provider test implement

---
+++ client/test/sqs/providers/sqs_service_provider_test.dart
@@ -0,0 +1,45 @@
+import 'package:aws_sqs_api/sqs-2012-11-05.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:localstack_dashboard_client/src/enums.dart';
+import 'package:localstack_dashboard_client/src/profiles/models/profile.dart';
+import 'package:localstack_dashboard_client/src/profiles/providers/profile_provider.dart';
+import 'package:localstack_dashboard_client/src/sqs/providers/sqs_service_provider.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'sqs_service_provider_test.mocks.dart';
+
+@GenerateMocks([UserProfileController])
+void main() {
+  late MockUserProfileController mockUserProfileController;
+
+  setUp(() {
+    mockUserProfileController = MockUserProfileController();
+    mockUserProfileController.addProfile(
+        alias: "alias",
+        profileType: SupportServiceTypes.aws,
+        accessKey: "accessKey",
+        secretAccessKey: "secretAccessKey",
+        region: "region",
+        isSelect: true);
+  });
+
+  test("sqs service provider init test", () {
+    final container = ProviderContainer(
+      overrides: [
+        profileControllerProvider.overrideWithValue(mockUserProfileController),
+      ],
+    );
+
+    when(mockUserProfileController.currentProfile).thenReturn(ModelProfile(
+        alias: "alias",
+        profileType: SupportServiceTypes.aws,
+        accessKey: "accessKey",
+        secretAccessKey: "secretAccessKey",
+        region: "region",
+        isSelect: true));
+
+    expect(container.read(sqsServiceProvider), isA<SQS>());
+  });
+}
