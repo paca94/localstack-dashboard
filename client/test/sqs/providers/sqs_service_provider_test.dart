f09b40a53bccfe1eb0c02f6feac7f20183189393
JihwanKim <jihwan.dev@gmail.com>
2022-09-12 19:47:25 +0900

    Update sqs_service_provider_test.dart

--- client/test/sqs/providers/sqs_service_provider_test.dart
+++ client/test/sqs/providers/sqs_service_provider_test.dart
@@ -1,5 +1,6 @@
 import 'package:aws_sqs_api/sqs-2012-11-05.dart';
 import 'package:cloud_dashboard_client/src/common/enums.dart';
+import 'package:cloud_dashboard_client/src/empty_aws/empty_sqs.dart';
 import 'package:cloud_dashboard_client/src/profiles/models/profile.dart';
 import 'package:cloud_dashboard_client/src/profiles/providers/profile_provider.dart';
 import 'package:cloud_dashboard_client/src/sqs/providers/sqs_service_provider.dart';
@@ -41,5 +42,23 @@ void main() {
         isSelect: true));
 
     expect(container.read(sqsServiceProvider), isA<SQS>());
+  });
+
+  test("sqs service provider init test for empty", () {
+    final container = ProviderContainer(
+      overrides: [
+        profileControllerProvider.overrideWithValue(mockUserProfileController),
+      ],
+    );
+
+    when(mockUserProfileController.currentProfile).thenReturn(ModelProfile(
+        alias: "alias",
+        profileType: SupportServiceTypes.empty,
+        accessKey: "accessKey",
+        secretAccessKey: "secretAccessKey",
+        region: "region",
+        isSelect: true));
+
+    expect(container.read(sqsServiceProvider), isA<EmptySQS>());
   });
 }
